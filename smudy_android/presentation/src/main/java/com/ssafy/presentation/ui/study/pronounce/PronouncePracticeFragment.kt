39b33d41f33be20e6b8283e094632b5868afb9b0
안성준 <hiahn777@gmail.com>
2024-05-09 09:20:45 +0900

    feat: 발음 교정 문제 화면 및 풀이 화면 개발

--- smudy_android/presentation/src/main/java/com/ssafy/presentation/ui/study/pronounce/PronouncePracticeFragment.kt
+++ smudy_android/presentation/src/main/java/com/ssafy/presentation/ui/study/pronounce/PronouncePracticeFragment.kt
@@ -4,16 +4,14 @@ import android.media.MediaPlayer
 import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
 import android.util.Log
 import android.view.View
 import androidx.annotation.RequiresApi
 import androidx.core.view.isVisible
-import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.ssafy.presentation.R
 import com.ssafy.presentation.base.BaseFragment
@@ -23,21 +21,26 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import java.io.File
+import java.io.FileInputStream
+import java.util.Locale
 
 
 @AndroidEntryPoint
 class PronouncePracticeFragment : BaseFragment<FragmentPronouncePracticeBinding>(
     { FragmentPronouncePracticeBinding.bind(it) }, R.layout.fragment_pronounce_practice
-) {
+), TextToSpeech.OnInitListener {
     private val parentViewModel: PronounceProblemViewModel by hiltNavGraphViewModels(R.id.nav_pronounce)
     private var recorder: MediaRecorder? = null
-    private var player: MediaPlayer?=null
-    private lateinit var file: File
+    private var recordPlayer: MediaPlayer? = null
+    private var ttsPlayer: MediaPlayer? = null
+    private lateinit var recordFile: File
+    private lateinit var ttsFile: File
+    private lateinit var tts: TextToSpeech
 
     @RequiresApi(Build.VERSION_CODES.S)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Log.e("TAG", "onViewCreated: $parentViewModel")
+        tts = TextToSpeech(_activity, this)
         initView()
         initObserve()
         initEvent()
@@ -68,23 +71,27 @@ class PronouncePracticeFragment : BaseFragment<FragmentPronouncePracticeBinding>
                 changeVisibility()
             }
             btnResetRecord.setOnClickListener {
-                startPlaying()
+                startRecordPlaying()
+            }
+            llTts.setOnClickListener {
+                startTtsPlaying()
             }
         }
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
     private fun startRecording() {
-        file = File(_activity.cacheDir, "recorde.3gp")
+        recordFile = File(_activity.cacheDir, "recorde.3gp")
         recorder = MediaRecorder(_activity)
             .apply {
                 setAudioSource(MediaRecorder.AudioSource.MIC)
                 setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
                 setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
-                setOutputFile(file)
+                setOutputFile(recordFile)
                 prepare()
             }
         recorder!!.start()
+        binding.dvRecordDrawing.clearVisualization()
         binding.dvRecordDrawing.startVisualizing(false)
     }
 
@@ -97,23 +104,65 @@ class PronouncePracticeFragment : BaseFragment<FragmentPronouncePracticeBinding>
         binding.dvRecordDrawing.stopVisualizing()
     }
 
-    private fun startPlaying() {
-        player = MediaPlayer()
+    private fun startRecordPlaying() {
+        recordPlayer = MediaPlayer()
             .apply {
-                setDataSource(file.absolutePath)
+                setDataSource(recordFile.absolutePath)
                 prepare()
             }
-        player?.setOnCompletionListener {
-            stopPlaying()
+        recordPlayer?.setOnCompletionListener {
+            stopRecordPlaying()
         }
-        player?.start()
+        recordPlayer?.start()
         binding.dvRecordDrawing.startVisualizing(true)
     }
 
-    private fun stopPlaying() {
-        player?.release()
-        player = null
+    private fun stopRecordPlaying() {
+        recordPlayer?.release()
+        recordPlayer = null
         binding.dvRecordDrawing.stopVisualizing()
+    }
+
+    private fun startTtsPlaying() {
+        ttsPlayer = MediaPlayer()
+            .apply {
+                setDataSource(ttsFile.absolutePath)
+                prepare()
+            }
+        ttsPlayer?.setOnCompletionListener {
+            stopTtsPlaying()
+        }
+        ttsPlayer?.start()
+        binding.dvTtsDrawing.startVisualizing(true)
+    }
+
+    private fun stopTtsPlaying() {
+        ttsPlayer?.release()
+        ttsPlayer = null
+        binding.dvTtsDrawing.stopVisualizing()
+    }
+
+    private fun saveTextAsAudioFile() {
+        val params = Bundle()
+        params.putString(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "")
+
+        tts.setOnUtteranceProgressListener(object : UtteranceProgressListener() {
+            override fun onStart(utteranceId: String?) {
+                // 시작할 때 호출
+            }
+
+            override fun onDone(utteranceId: String?) {
+                // 파일 생성 완료 후 재생 준비
+                readWavFile(ttsFile.absolutePath)
+            }
+
+            override fun onError(utteranceId: String?) {
+
+            }
+
+        })
+
+        tts.synthesizeToFile(parentViewModel.translateLyric.value[0], params, ttsFile, "UniqueID")
     }
 
     private fun setMusicView(pronounce: PronounceProblem) {
@@ -140,5 +189,46 @@ class PronouncePracticeFragment : BaseFragment<FragmentPronouncePracticeBinding>
                 setLyricView(it)
             }
         }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.language = Locale.US
+            ttsFile = File(_activity.cacheDir, "tts_output.wav")
+            saveTextAsAudioFile()
+        }
+    }
+
+    fun readWavFile(filePath: String) {
+        val file = File(filePath)
+        val inputStream = FileInputStream(file)
+
+        // WAV 헤더 스킵 (44 바이트 일반적)
+        val header = ByteArray(44)
+        inputStream.read(header)
+
+        // 샘플 레이트 및 데이터 읽기
+        val sampleRate = 44100  // 예: 44100Hz
+        val bytesPerSample = 2  // 16비트 PCM
+        val durationPerChunk = 20 // ms
+        val numSamplesPerChunk = (sampleRate / 1000) * durationPerChunk
+
+        val buffer = ByteArray(numSamplesPerChunk * bytesPerSample)
+        var bytesRead: Int
+        var maxAmplitude: Short = 0
+
+        while (inputStream.read(buffer).also { bytesRead = it } != -1) {
+            for (i in 0 until bytesRead step 2) {
+                val amplitude =
+                    ((buffer[i + 1].toInt() shl 8) or (buffer[i].toInt() and 0xff)).toShort()
+                if (amplitude > maxAmplitude) maxAmplitude = amplitude
+            }
+            Log.e("TAG", "readWavFile: $maxAmplitude")
+            binding.dvTtsDrawing.onRequestCurrentAmplitude = { maxAmplitude.toInt() }
+            binding.dvTtsDrawing.startVisualizing(false)
+            maxAmplitude = 0  // reset for the next chunk
+        }
+        binding.dvTtsDrawing.stopVisualizing()
+        inputStream.close()
     }
 }
--- smudy_android/presentation/src/main/java/com/ssafy/presentation/ui/study/pronounce/PronounceProblemFragment.kt
+++ smudy_android/presentation/src/main/java/com/ssafy/presentation/ui/study/pronounce/PronounceProblemFragment.kt
@@ -2,12 +2,7 @@ package com.ssafy.presentation.ui.study.pronounce
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -17,7 +12,6 @@ import com.bumptech.glide.Glide
 import com.ssafy.presentation.R
 import com.ssafy.presentation.base.BaseFragment
 import com.ssafy.presentation.base.BaseHolder
-import com.ssafy.presentation.databinding.FragmentPronouncePracticeBinding
 import com.ssafy.presentation.databinding.FragmentPronounceProblemBinding
 import com.ssafy.presentation.ui.study.adapter.PronounceLyricAdapter
 import dagger.hilt.android.AndroidEntryPoint
