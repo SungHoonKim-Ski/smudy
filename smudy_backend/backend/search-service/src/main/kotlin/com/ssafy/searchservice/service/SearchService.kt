4b021c1ef6ff91c812bfbb9b69a0cb26625e1baa
ukyoung147 <ukyoung147@gmail.com>
2024-05-09 10:34:04 +0900

    Fix: 검색 쿼리문 수정
    - String Query -> Criteria Query 방식으로 수정

--- smudy_backend/backend/search-service/src/main/kotlin/com/ssafy/searchservice/service/SearchService.kt
+++ smudy_backend/backend/search-service/src/main/kotlin/com/ssafy/searchservice/service/SearchService.kt
@@ -1,9 +1,11 @@
 package com.ssafy.searchservice.service
 
 import com.ssafy.searchservice.entity.SongDocument
+import org.springframework.data.domain.PageRequest
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations
+import org.springframework.data.elasticsearch.core.query.Criteria
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery
 import org.springframework.data.elasticsearch.core.query.Query
-import org.springframework.data.elasticsearch.core.query.StringQuery
 import org.springframework.stereotype.Service
 
 @Service
@@ -13,33 +15,13 @@ class SearchService(
 
     fun searchAutoCreation(query: String): MutableList<SongDocument> {
 
-        val sQuery: Query = StringQuery("""
-            {
-                "bool": {
-                    "must": [
-                        {
-                            "match": {
-                                "song_name": {
-                                    "query": "$query",
-                                    "boost": 2
-                                }
-                            }
-                        }
-                    ],
-                    "should": [
-                        {
-                            "match": {
-                                "song_lyrics": "$query"
-                            }
-                        }
-                    ],
-                    "minimum_should_match": 1
-                }
-            }
-        """
-        )
 
-        return elasticsearchOperations.search(sQuery, SongDocument::class.java)
+        val mustCriteria = Criteria("song_name").`is`(query).boost(2.0f)
+        val shouldCriteria = Criteria("song_lyrics").`is`(query)
+        val criteria = mustCriteria.and(shouldCriteria)
+        val criteriaQuery: Query = CriteriaQuery(criteria)
+
+        return elasticsearchOperations.search(criteriaQuery, SongDocument::class.java)
             .map { it.content }
             .toList()
     }
@@ -49,34 +31,20 @@ class SearchService(
         val pageSize = 10 // 한 페이지 당 결과 수, 필요에 따라 조정 가능
         val from = (page - 1) * pageSize // 페이징을 위한 시작 인덱스 계산
 
-        val sQuery: Query = StringQuery("""
-            {
-                "bool": {
-                    "must": [
-                        {
-                            "match": {
-                                "song_name": {
-                                    "query": "$query",
-                                    "boost": 2
-                                }
-                            }
-                        }
-                    ],
-                    "should": [
-                        {
-                            "match": {
-                                "song_lyrics": "$query"
-                            }
-                        }
-                    ],
-                    "minimum_should_match": 1
-                }
-            },
-            "from": $from,
-            "size": $pageSize
-        """)
+        // 개별 Criteria 생성
+        val mustQ = Criteria.where("song_name").`is`(query).boost(2.0f)
+        val artistQ = Criteria.where("song_artist").`is`(query)
+        val lyricsQ = Criteria.where("song_lyrics").`is`(query)
+        val shouldQ = artistQ.or(lyricsQ)
 
-        return elasticsearchOperations.search(sQuery, SongDocument::class.java)
+        // boolean 쿼리 생성
+        val booleanQ = mustQ.and(shouldQ)
+
+        // 쿼리 객체를 생성, 페이징 설정을 추가
+        val criteriaQuery: Query =
+            CriteriaQuery(booleanQ).setPageable(PageRequest.of(from, pageSize))
+
+        return elasticsearchOperations.search(criteriaQuery, SongDocument::class.java)
             .map { it.content }
             .toList()
     }
