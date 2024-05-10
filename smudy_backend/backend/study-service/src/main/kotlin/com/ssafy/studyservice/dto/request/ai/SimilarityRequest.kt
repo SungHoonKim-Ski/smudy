59837fd95ae6a7c8caa2033b480769d382b13786
ksh7412 <ksh7412.dev@gmail.com>
2024-05-10 11:09:33 +0900

    feat: ai 서버 테스트중

--- smudy_backend/backend/study-service/src/main/kotlin/com/ssafy/studyservice/dto/request/ai/SimilarityRequest.kt
+++ smudy_backend/backend/study-service/src/main/kotlin/com/ssafy/studyservice/dto/request/ai/SimilarityRequest.kt
@@ -1,6 +1,6 @@
 package com.ssafy.studyservice.dto.request.ai
 
 data class SimilarityRequest (
-        private val sentence1: String,
-        private val sentence2: String,
+        private val sentence1: String = "hello world",
+        private val sentence2: String = "hello",
 )
