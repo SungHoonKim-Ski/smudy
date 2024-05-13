5091d0b7fe26baf09993b3a6d7be64b32c56e056
김성훈 <ksh7412.dev@gmail.com>
2024-05-13 17:40:25 +0900

    fix: ai 서버 테스트중..

--- smudy_backend/backend/study-service/src/main/kotlin/com/ssafy/studyservice/dto/response/ai/Similarity.kt
+++ smudy_backend/backend/study-service/src/main/kotlin/com/ssafy/studyservice/dto/response/ai/Similarity.kt
@@ -3,6 +3,9 @@ package com.ssafy.studyservice.dto.response.ai
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Similarity(
+        @get: JsonProperty("new_sentence")
         val newSentence: String,
+
+        @get: JsonProperty("cosine_similarity")
         val cosineSimilarity: Double
 )
