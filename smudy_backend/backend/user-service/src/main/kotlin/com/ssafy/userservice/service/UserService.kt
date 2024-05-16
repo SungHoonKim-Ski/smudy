cede267154cffb712e6f815ddf151176feebb51e
김성훈 <ksh7412.dev@gmail.com>
2024-05-16 17:14:24 +0900

    feat: 틀린 문제 랜덤 제공 api 기능 개발, pronounce부분 스트릭 기능 추가

--- smudy_backend/backend/user-service/src/main/kotlin/com/ssafy/userservice/service/UserService.kt
+++ smudy_backend/backend/user-service/src/main/kotlin/com/ssafy/userservice/service/UserService.kt
@@ -21,6 +21,7 @@ class UserService(
         private val songService: SongService,
         private val learnReportService: LearnReportService,
         private val studyServiceClient: StudyServiceClient,
+        private val wrongService: WrongService,
         private val aiService: AiService,
 ) {
 
@@ -82,6 +83,7 @@ class UserService(
         val song = songService.findSongBySongId(request.songId)
         val answers = song.songLyrics
         val userAnswer = request.userWords.map { it.word }
+        val wrongs = mutableListOf<Wrong>()
 
         var score = 0
         var totalSize = 0
@@ -98,12 +100,21 @@ class UserService(
 
                         val isCorrect = userAnswer[index] == lyric.lyricBlankAnswer
                         if (isCorrect) score++
-
+                        else {
+                            wrongs.add(
+                                    Wrong(
+                                            userInternalId = userInternalId,
+                                            lyricSentenceEn = lyric.lyricSentenceEn,
+                                            lyricSentenceKo = lyric.lyricSentenceEn
+                                    )
+                            )
+                        }
                         isCorrect
                     }
                 )
             )
         }
+
 
         val userLearnReport = learnReportService.saveLearnReport(
                 LearnReport(
@@ -158,6 +169,8 @@ class UserService(
         val song = songService.findSongBySongId(request.songId)
         val response = SubmitPickResponse()
 
+        val wrongs = mutableListOf<Wrong>()
+
         problems.forEachIndexed { index, problem ->
             if (problem.sentenceEn == request.userPicks[index].userPick) {
                 response.correct.add(
@@ -168,13 +181,28 @@ class UserService(
                 )
                 response.score++
             } else {
-                response.wrong.add(WrongProblem(
-                        userLyricSentence = request.userPicks[index].userPick,
-                        lyricSentenceEn = problem.sentenceEn,
-                        lyricSentenceKo = problem.sentenceKo,
-                ))
+                response.wrong.add(
+                        WrongProblem(
+                            userLyricSentence = request.userPicks[index].userPick,
+                            lyricSentenceEn = problem.sentenceEn,
+                            lyricSentenceKo = problem.sentenceKo,
+                        )
+                )
             }
         }
+
+        response.wrong.map {
+            wrongs.add(
+                    Wrong(
+                            userInternalId = userInternalId,
+                            lyricSentenceEn = it.lyricSentenceEn,
+                            lyricSentenceKo = it.lyricSentenceKo,
+                    )
+            )
+        }
+
+        wrongService.saveAll(wrongs)
+
 
         val userLearnReport = learnReportService.saveLearnReport(
                 LearnReport(
@@ -220,6 +248,7 @@ class UserService(
 
         val userExpresses = request.userExpresses
         val song = songService.findSongBySongId(request.songId)
+        val wrongs = mutableListOf<Wrong>()
 
         val userLearnReport = learnReportService.saveLearnReport(
                 LearnReport(
@@ -247,6 +276,20 @@ class UserService(
                 )
         )
 
+        userExpresses.forEachIndexed { index, userExpress ->
+            if (userExpress.score < 70) {
+                wrongs.add(
+                        Wrong(
+                                userInternalId = userInternalId,
+                                lyricSentenceKo = userExpress.suggestLyricSentence,
+                                lyricSentenceEn = userExpress.suggestLyricSentence,
+                        )
+                )
+            }
+        }
+
+        wrongService.saveAll(wrongs)
+
         val isCorrect = addStreak(
                 average = userLearnReport.learnReportScore,
                 userInternalId = userInternalId,
@@ -262,6 +305,7 @@ class UserService(
         return "Express 제출 완료"
     }
 
+    @Transactional
     fun analyzeAndSavePronounce(
             userInternalId: UUID,
             request: SubmitPronounceRequest,
@@ -272,8 +316,6 @@ class UserService(
         val analyzeResponse = getPronounceAnalyze(ttsFile = ttsFile, userFile = userFile)
 
         val mapper = ObjectMapperConfig().getObjectMapper()
-
-//        val parseAnalyze = mapper.readValue(mapper.writeValueAsString(analyzeResponse), EntityLyricAiAnalyze::class.java)
 
         val learnReportPronounce = LearnReportPronounce(
                 learnReportId = -1,
@@ -288,6 +330,19 @@ class UserService(
                 songId = request.songId,
                 userInternalId = userInternalId
         )
+
+        val song = songService.findSongBySongId(request.songId)
+
+        addStreak(
+                average = 100,
+                userInternalId = userInternalId,
+                albumJacket = song.albumJacket
+        )
+
+        userRepository.findByUserInternalId(userInternalId)?.let {
+            it.userExpUp()
+        } ?: throw UserNotFoundException("Pronounce 채점 도중 유저를 찾지 못함")
+
 
         return SubmitPronounceResponse(
                 lyricSentenceEn = request.lyricSentenceEn,
