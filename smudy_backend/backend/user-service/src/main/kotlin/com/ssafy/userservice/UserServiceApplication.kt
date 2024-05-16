7d3d7c77dea44c8f82c8be8d48638abe9f23851a
김성훈 <ksh7412.dev@gmail.com>
2024-05-16 14:02:03 +0900

    fix: timezone 맞추는중

--- smudy_backend/backend/user-service/src/main/kotlin/com/ssafy/userservice/UserServiceApplication.kt
+++ smudy_backend/backend/user-service/src/main/kotlin/com/ssafy/userservice/UserServiceApplication.kt
@@ -2,11 +2,13 @@ package com.ssafy.userservice
 
 import com.ssafy.userservice.exception.error.FeignErrorDecoder
 import feign.Logger
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Bean
+import java.util.*
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -21,6 +23,12 @@ fun main(args: Array<String>) {
 fun feignLoggerLever(): Logger.Level {
 	return Logger.Level.FULL
 }
+
+@PostConstruct
+fun init() {
+	TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+}
+
 
 //@Bean
 //fun getFeignErrorDecoder(): FeignErrorDecoder {
